55cdabd721a72ebb2fba651c31eccdf9e48b0dd0
LakelanJonesThales <lakelan.jones@students.thalesacademy.org>
2022-10-26 16:56:14 -0400

    Completed teleop as far as is possible w/o a robot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele.java
@@ -41,50 +41,66 @@ public class Tele extends OpMode {
         if (gamepad1.atRest()) robot.rest();
         else {
             //If the gamepad is NOT at rest, then we want to see what we need to do.
+            //GAMEPAD 1 CODE
             robot.mechanumMovT(gamepad1.left_stick_x, -gamepad1.left_stick_y, gamepad1.right_stick_x);
                 
-            int leftY = gamepad1.left_stick_y;
-            int leftX = gamepad1.left_stick_x;
-            int rightY = gamepad1.right_stick_y;
-            int rightX = gamepad1.right_stick_x;
+            double leftY = gamepad1.left_stick_y;
+            double leftX = gamepad1.left_stick_x;
+            Double rightY = gamepad1.right_stick_y;
+            double rightX = gamepad1.right_stick_x;
+            
             
             //If left stick is held to the left, move left.
             if (gamepad1.left_stick_x < -0.2) {
-                 robot.moveLeft(leftX);
+                 robot.moveLeft(Math.abs(leftX));
             }
             //If left stick is held to the right, move right.
             if (gamepad1.left_stick_x > 0.2) {
-                robot.moveRight(leftX);            
+                robot.moveRight(Math.abs(leftX));            
             }
             //If left stick is held forward, move forward.
             if (gamepad1.left_stick_y > 0.2) {
-                robot.moveForward(leftY);
+                robot.moveForward(Math.abs(leftY));
             }
             //If left stick is held back, move backward.
             if (gamepad1.left_stick_y < -0.2) {
-                robot.moveBackward(leftY);
+                robot.moveBackward(Math.abs(leftY));
             }
             //if right stick is held left, turn left.
             if (gamepad1.right_stick_x < -0.2) {
-                robot.turnLeft(rightX);
+                robot.turnLeft(Math.abs(rightX));
             }
             //if right stick is held right, turn right.
             if (gamepad1.right_stick_x > 0.2) {
-                robot.turnRight(rightX);
+                robot.turnRight(Math.abs(rightX));
+            }
+            
+            //button mapping for quick turning
+            if (gamepad1.x) {
+                robot.turnRightFT(1300,0.5);
+            }
+            if (gamepad1.a) {
+                robot.turnRightFT(2600,0.5);
+            }
+            if (gamepad1.b) {
+                robot.turnLeftFT(1300,0.5);
+            }
+            if (gamepad1.y) {
+                robot.turnLeftFT(2600,0.5);
             }
 
-        
-            //arm/claw code kinda since we don't remember how to
-
-            int leftY2 = gamepad2.left_stick_y;
+            //GAMEPAD 2 CODE
+            double leftY2 = gamepad2.left_stick_y;
+            double rightY2 = gamepad2.right_stick_y;
+            double leftX2 = gamepad2.left_stick_x;
+            double rightX2 = gamepad2.right_stick_x;
 
             // the arm moves up
             if (gamepad2.left_stick_y > 0.2) {
-                armMotor(leftY2);
+              armMotor(leftY2);
             }
             
             // the arm moves down
-            
             if (gamepad2.left_stick_y < -0.2) {
                 armMotor(leftY2);
             }
@@ -93,17 +109,36 @@ public class Tele extends OpMode {
             // the claw opens
 
             if (gamepad2.left_trigger) {
-                clawServo(1);
+                clawServo(0);
             }
 
 
             // the claw closes
 
             if (gamepad2.right_trigger) {
-                clawServo(0);
+                clawServo(180);
+            }
+           //reset 
+            if (gamepad2.x) {
+             armMotor();//reset
             }
 
+            //tall junction preset
+            if (gamepad2.x) {
+                armMotor();//however much it needs to go to preset tall junction
+            }
             
+            //medium junction preset
+            if (gamepad2.b) {
+                armMotor();//however much it needs to go to preset medium junction
+            }
+            
+            //small junction preset
+            if (gamepad2.a) {
+                armMotor();//however much it needs to go to preset small junction
+            }
+
+
         }
     }
 }
