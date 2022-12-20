21b29b5d53a15820934534170959ac36fc049350
LakelanJonesThales <lakelan.jones@students.thalesacademy.org>
2022-12-20 13:19:53 -0500

    fixed arm code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueAuton.java
@@ -8,6 +8,10 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 import static org.firstinspires.ftc.teamcode.Spark.Drivetrain.MECHANUM;
 
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.RestoreAction;
+import javax.swing.text.html.parser.ContentModel;
+import javax.xml.catalog.GroupEntry.ResolveType;
+
 
 @Autonomous(name="BlueAuton", group="Template")
 @Disabled 
@@ -26,47 +30,47 @@ public class BlueAuton extends LinearOpMode {
         //Any code that goes in this space is only run once, and after it is finished the program ends.
 
 
-        int R = 250; //sleep for 250 milliseconds
-        int T = 650; //45 degree turn
-        int M = 20; //1 cm
+        int restTicks = 250; //sleep for 250 milliseconds
+        int cmTicks = 20; //1 cm
+        int degTicks = 650; //45 degree turn
 
         //robot code for bottom left and top right   
 
-        robot.moveRightFT(M * 60, 0.5);
-        sleep(R);
-        robot.moveBackwardFT(M * 90, 0.5);
-        sleep(R);
-        robot.turnRightFT(T, 0.5);
-        sleep(R);
-        //robot moves its arm to hig joycon
+        robot.moveRightFT(cmTicks * 60, 0.5);
+        sleep(restTicks);
+        robot.moveBackwardFT(cmTicks * 90, 0.5);
+        sleep(restTicks);
+        robot.turnRightFT(degTicks, 0.5);
+        sleep(restTicks);
+        //robot moves its arm to high junction
         robot.armUpFT(180, 0.5);
 
         for (int a = 0; a < 2; a++) {
 
             //robot moves to get cone
-            robot.moveLeftFT(M * 90, 0.5);
-            sleep(R);
-            robot.turnLeftFT(T, 0.5);
-            sleep(R);
+            robot.moveLeftFT(cmTicks * 90, 0.5);
+            sleep(restTicks);
+            robot.turnLeftFT(degTicks, 0.5);
+            sleep(restTicks);
             //claw open
             //robot.clawServo(0);
 
-            robot.turnLeftFT(T * 3, 0.5);
-            sleep(R);
-            robot.moveRightFT(M * 90, 0.5);
-            //robot move medium juncti
+            robot.turnLeftFT(degTicks * 3, 0.5);
+            sleep(restTicks);
+            robot.moveRightFT(cmTicks * 90, 0.5);
+            //robot move medium junction
             robot.armUpFT(145, 0.5);
 
             //
-            robot.moveLeftFT(M * 90, 0.5);
-            sleep(R);
-            robot.turnRightFT(T * 3, 0.5);
+            robot.moveLeftFT(cmTicks * 90, 0.5);
+            sleep(restTicks);
+            robot.turnRightFT(degTicks * 3, 0.5);
             //claw open
             //robot.clawServo(0);
 
-            robot.turnRightFT(T * 3, 0.5);
-            sleep(R);
-            robot.moveLeftFT(M * 90, 0.5);
+            robot.turnRightFT(degTicks * 3, 0.5);
+            sleep(restTicks);
+            robot.moveLeftFT(cmTicks * 90, 0.5);
 
             //high junction
             robot.armUpFT(180, 0.5);
